14bd2b33e6085c77e8132f7ee4d9e55795fbe16a
campbell-purdie <campbell.purdie@hotmail.com>
2025-09-09 15:30:45 +1000

    money

--- k8s/run_kubernetes.sh
+++ k8s/run_kubernetes.sh
@@ -13,7 +13,7 @@ kubectl apply -f mysql-service.yml
 kubectl apply -f phpmyadmin-deployment.yml
 kubectl apply -f phpmyadmin-service.yml
 
-kubectl create configmap nginx-config --from-file=nginx.conf -o yaml --dry-run=client | kubectl apply -f -
+kubectl create configmap nginx-config --from-file=default.conf -o yaml --dry-run=client | kubectl apply -f -
 kubectl apply -f nginx-deployment.yml
 kubectl apply -f nginx-service.yml
 
